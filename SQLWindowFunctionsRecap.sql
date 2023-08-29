96193a75f3842fb29ca9a8d9748212be12805f8a
drmehmetyegin <dr.mehmetyegin@gmail.com>
2023-08-30 00:00:03 +0200

    Update SQLWindowFunctionsRecap.sql

--- SQLWindowFunctionsRecap.sql
+++ SQLWindowFunctionsRecap.sql
@@ -312,4 +312,24 @@ SELECT DISTINCT order_id,
 		AVG((list_price*quantity*(1-discount))) OVER (PARTITION BY order_id) as avg_ord_price
 FROM sale.order_item
 
+--////////////////////////
 
+--QUESTION: Which orders' average product price is lower than the overall average price?
+
+
+SELECT DISTINCT order_id, avg_ord_price
+FROM (SELECT order_id, 
+		AVG((list_price*quantity*(1-discount))) OVER () as avg_pr_price,
+		AVG((list_price*quantity*(1-discount))) OVER (PARTITION BY order_id) as avg_ord_price
+FROM sale.order_item
+) t
+WHERE avg_pr_price > avg_ord_price
+ORDER BY avg_ord_price DESC
+
+
+--////////////////////////
+
+--QUESTION: Calculate the stores' weekly cumulative count of orders for 2018.
+
+SELECT *
+FROM Sale.orders
